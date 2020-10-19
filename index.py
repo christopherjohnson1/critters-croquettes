f47eae226f62cd37db7d5a6ce79aaa4ae2d166cc
Chris Johnson <christopherjohnson1@gmail.com>
2020-10-19 16:04:36 -0400

    petting zoo habitat works

--- index.py
+++ index.py
@@ -1,7 +1,15 @@
 from flying import Cardinal, Eagle, Osprey, Sandpiper, Swan
 from swimming import BlueWhale, Flounder, Salmon, Shark, Tuna
 from walking import Capybara, Cheetah, Elephant, RedFox, Tiger
+from habitats import PettingZoo
+
+varmint_village = PettingZoo("Varmint Village")
 
 carlos = Capybara("Carlos", "Capybara", "morning", "cat food")
+frank = Flounder("Frank", "Flounder", "algae")
 
-print(carlos)
+varmint_village.animals.append(carlos)
+varmint_village.animals.append(frank)
+
+for animal in varmint_village.animals:
+    print(f'You can find {animal.name} the {animal.species} in {varmint_village.attraction_name}')
